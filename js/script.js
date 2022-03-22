3b3d83b8f85f7be8973400ca48f7d16ebdda8dad
unknown <alifouda1474@gmail.com>
2022-03-22 17:24:30 +0000

    New Version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const searchButton = document.getElementById("search-icon");
 let searchBar = document.querySelector(".search-bar");
+let to_top_btn  = document.querySelector(".scrol-top-button");
 searchButton.onclick=function(){
  if (searchBar.classList.contains("animation")){
      searchBar.classList.remove("animation")
@@ -9,4 +10,15 @@ searchButton.onclick=function(){
  }
 
 
+}
+window.onscroll=function(){
+   if (this.scrollY>300){
+       to_top_btn.classList.add("d-flex")
+   }
+   else{
+   to_top_btn.classList.remove("d-flex")
+   }
+}
+to_top_btn.onclick=function(){
+   window.scrollTo(0,0)
 }
